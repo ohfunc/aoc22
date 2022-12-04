1558d4801291c29fb2ccb9746bd0eac3fa96fd17
Nick Bellamy <nicholas.bellamy@live.com>
2022-12-04 02:01:34 +0000

    .gitignore

--- day2/target/debug/deps/day2-b8cdf47dc9aa4868.d
+++
@@ -1,5 +0,0 @@
-/home/bell/advent2022/day2/target/debug/deps/day2-b8cdf47dc9aa4868.rmeta: src/main.rs
-
-/home/bell/advent2022/day2/target/debug/deps/day2-b8cdf47dc9aa4868.d: src/main.rs
-
-src/main.rs:
--- day2/target/debug/deps/day2-f06b94f7ee39ac14.d
+++
@@ -1,5 +0,0 @@
-/home/bell/advent2022/day2/target/debug/deps/day2-f06b94f7ee39ac14: src/main.rs
-
-/home/bell/advent2022/day2/target/debug/deps/day2-f06b94f7ee39ac14.d: src/main.rs
-
-src/main.rs:
